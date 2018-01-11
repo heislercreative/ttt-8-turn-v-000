2167903f4586001d3c884241ed6565d3fc32ede3
Brennan Heisler <learn@heislercreative.com>
2018-01-11 00:46:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,3 +14,11 @@ end
 def position_taken?(board, index)
   board[index] != " " && board[index] != "" && board[index] != nil
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
